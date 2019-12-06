91cb2067ed849bac253450afdf9d8375625c8786
Levinuk <levin_joller@sluz.ch>
2019-12-06 17:07:15 +0100

    Modified server.js to return the index.html page on request

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express');
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.sendStatus(200);
-})
+app.use("/", express.static("./"));
 
 app.listen(3333, () => {
     console.log("server is runnig on port: 3333");
